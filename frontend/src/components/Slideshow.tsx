4960faf75a484ff86df9f11ff9bac452c9b709de
Its-utsav <utsavdhimmar987@gmail.com>
2025-07-01 18:38:14 +0530

    Inconsolata font and missing key propertie in Slideshow

--- frontend/src/components/Slideshow.tsx
+++ frontend/src/components/Slideshow.tsx
@@ -7,7 +7,7 @@ export default function Slideshow({ imageUrls }: SlideshowProps) {
         <div className="carousel h-3/3 max-w-md carousel-center space-x-4 rounded-box bg-neutral p-4">
             {Array.isArray(imageUrls) ? (
                 imageUrls.map((img) => (
-                    <div className="carousel-item">
+                    <div className="carousel-item" key={img}>
                         <img src={img} className="rounded-box" />
                     </div>
                 ))
